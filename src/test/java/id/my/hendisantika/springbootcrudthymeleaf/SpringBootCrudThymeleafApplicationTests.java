990e7552a1d01bcde01a7c1c281390686b0f21f8
hendisantika <hendisantika@yahoo.co.id>
2024-07-02 19:34:27 +0700

    Create Spring Boot Project🫸🌀✏️📗🐧🐳

---
+++ src/test/java/id/my/hendisantika/springbootcrudthymeleaf/SpringBootCrudThymeleafApplicationTests.java
@@ -0,0 +1,13 @@
+package id.my.hendisantika.springbootcrudthymeleaf;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootCrudThymeleafApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
